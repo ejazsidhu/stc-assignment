6c02bffc8650d21f834f4f458a4cc6998d18e605
ejaz-tapestry <eurrahman@tapestry.com>
2023-09-11 20:49:19 +0500

    langualge changer added to admin

--- src/app/auth/pages/admin/admin-base/admin-base.component.ts
+++ src/app/auth/pages/admin/admin-base/admin-base.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from 'src/app/interfaces/store.interface';
 import { StoreService } from 'src/app/services/store/store.service';
+import { TranslateHelperService } from 'src/app/services/translate/translate-helper.service';
 
 @Component({
   selector: 'app-admin-base',
@@ -12,8 +13,10 @@ import { StoreService } from 'src/app/services/store/store.service';
 export class AdminBaseComponent {
 
   _store: Store;
+  // languageSelection:string = 'ar';
+  // languageList: string[] = ['ar','en'];
 
-  constructor(private router: Router, private store: StoreService) {
+  constructor(public langHelper:TranslateHelperService,private router: Router, private store: StoreService) {
     this._store = this.store.getState();
   }
 
